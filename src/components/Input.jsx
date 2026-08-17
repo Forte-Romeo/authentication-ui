8f8ff03142c3fd1e0da8348559c1516714d5950b
Forte-Romeo <fergusonromeonyarmah@gmail.com>
2026-08-17 13:13:40 +0000

    add: password visibility

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,15 +1,32 @@
-function Input({ label, type="text", placeholder, id, value, onChange }) {
+function Input({ label, type="text", placeholder, id, value, onChange, showPassword, onTogglePassword }) {
+    const inputType =
+        type === "password" && showPassword
+            ? "text"
+            : type;
+    
     return (
         <div className="form-group">
             <label htmlFor={id}>{label}</label>
 
-            <input 
-                type={type}
+            <div className="input-wrapper">
+                <input 
+                type={inputType}
                 id={id}
                 placeholder={placeholder}
                 value={value}
                 onChange={onChange}
             />
+
+            {type === "password" && (
+                <button
+                    type="button"
+                    className="password-toggle"
+                    onClick={onTogglePassword}
+                >
+                    {showPassword ? "Hide" : "Show"}
+                </button>
+            )}
+            </div>
         </div>
     );
 }
