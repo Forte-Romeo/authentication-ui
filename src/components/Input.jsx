9b56bfd4440e0709aaf3e856910107df534b7f48
Forte-Romeo <fergusonromeonyarmah@gmail.com>
2026-08-17 12:01:17 +0000

    add: reusable Input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,14 @@
+function Input({ label, type, placeholder }) {
+    return (
+        <div className="form-group">
+            <label>{label}</label>
+
+            <input 
+                type={type}
+                placeholder={placeholder}
+            />
+        </div>
+    );
+}
+
+export default Input;
